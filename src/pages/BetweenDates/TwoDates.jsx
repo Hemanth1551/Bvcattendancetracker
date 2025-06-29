6b637c750e92b833fb42bdb28b91451a3d2568e1
Hemanth_ Dommeti <94308036+Hemanth1551@users.noreply.github.com>
2025-06-29 19:19:53 +0530

    Update TwoDates.jsx

--- src/pages/BetweenDates/TwoDates.jsx
+++ src/pages/BetweenDates/TwoDates.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <PageMeta
-        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        title="HD EduTime"
         description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6 bg-white dark:bg-gray-900 min-h-screen text-gray-800 dark:text-gray-100">
