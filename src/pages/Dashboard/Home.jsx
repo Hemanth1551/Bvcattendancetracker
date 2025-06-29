f90aeb1f9eb91372e3a409dd5c638611d48a52f7
Hemanth_ Dommeti <94308036+Hemanth1551@users.noreply.github.com>
2025-06-29 19:20:19 +0530

    Update Home.jsx

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -97,7 +97,7 @@ export default function Home() {
   return (
     <>
       <PageMeta
-        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        title="HD EduTime"
         description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
 
