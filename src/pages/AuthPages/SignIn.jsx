402d0bcfac15783ef50f2e78cd710f0d0eec4c09
Hemanth_ Dommeti <94308036+Hemanth1551@users.noreply.github.com>
2025-07-01 22:16:55 +0530

    Update SignIn.jsx

--- src/pages/AuthPages/SignIn.jsx
+++ src/pages/AuthPages/SignIn.jsx
@@ -6,7 +6,7 @@ export default function SignIn() {
   return (
     <>
       <PageMeta
-        title="HD EduTime"
+        title="BVC's Attendance System"
         description="This is React.js SignIn Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
       <AuthLayout>
