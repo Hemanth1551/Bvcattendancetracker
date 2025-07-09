ae2270f04c716749c3bf9aa37e8b341c5857d0f1
Hemanth_ Dommeti <94308036+Hemanth1551@users.noreply.github.com>
2025-07-09 07:08:31 +0530

    Update days.js

--- src/api/days.js
+++ src/api/days.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/day",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/day`,
 });
 
 export const addDay = async (formData) => {
