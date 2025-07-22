e2ab771841ce4075164f8443dce2edc3eeefe503
Hemanth_ Dommeti <94308036+Hemanth1551@users.noreply.github.com>
2025-07-22 18:45:59 +0530

    Update DefaultInputs.jsx

--- src/components/form/form-elements/DefaultInputs.jsx
+++ src/components/form/form-elements/DefaultInputs.jsx
@@ -56,6 +56,21 @@ export default function DefaultInputs() {
       localStorage.setItem("deviceId", did);
     }
     setDeviceId(did);
+
+    const ua = navigator.userAgent;
+    const isChrome =
+      /Chrome/.test(ua) &&
+      /Google Inc/.test(navigator.vendor) &&
+      !/Edg/.test(ua) &&
+      !/OPR/.test(ua);
+
+    if (!isChrome) {
+      setAlert({
+        variant: "error",
+        title: "Attendance posting Failed",
+        message: "Please prefer your own mobile device using Google Chrome browser.",
+      });
+    }
   }, []);
 
   useEffect(() => {
